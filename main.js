c95fcdf5c06813a594f5ec1ba1f6bad14d3c9d14
Mahesh Pratap <maheshpratap@MacBook-Air-2.local>
2019-03-23 13:03:32 +0530

    final code

--- main.js
+++ main.js
@@ -1,12 +1,32 @@
 (function(){
   const subscribeBtn = document.getElementsByClassName('btn-subscribe')[0];
   const subscribeTxt = document.getElementsByClassName('form-control')[0];
-  subscribeBtn.addEventListener('click', (event) => {
+  subscribeBtn.addEventListener('click', function() {
     subscribeTxt.classList.toggle('expand');
-    if(event.target.innerText === 'JOIN US'){
-      event.target.innerText = 'SUBMIT';
-    } else {
-      event.target.innerText = 'JOIN US';
-    }
-  })  
+    this.innerText === 'I\'m in' ? this.innerText = 'Submit' : this.innerText = 'Thanks';
+  });
+  const fetchAPI = function() {
+    fetch('https://randomuser.me/api/')
+    .then(
+      function(response) {
+        if (response.status !== 200) {
+          console.log('Looks like there was a problem. Status Code: ' +
+            response.status);
+          return;
+        }
+  
+        // Examine the text in the response
+        response.json().then(function(data) {
+          return data.results[0];
+        });
+      }
+    )
+    .catch(function(err) {
+      console.log('Fetch Error :-S', err);
+    });
+  }
+
+  const image  = fetchAPI();
+  console.log(image)
+
 })();
